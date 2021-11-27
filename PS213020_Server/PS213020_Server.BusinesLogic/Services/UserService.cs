6b42d4053a35aca27468471853001371a9a738e2
StudentDev <vovaklinov007@gmail.com>
2021-11-27 17:49:33 +0300

    124
    12356

---
+++ PS213020_Server/PS213020_Server.BusinesLogic/Services/UserService.cs
@@ -0,0 +1,38 @@
+﻿using PS213020_Server.BusinesLogic.Core.Interfaces;
+using PS213020_Server.BusinesLogic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS213020_Server.BusinesLogic.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<UserInformationBLo> Auth(int PhoneNumberPrefix, int PhoneMumber, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DoesExist(int PhoneNumberPrefix, int PhoneMumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInformationBLo> Get(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserInformationBLo> Regestration(int PhoneNumberPrefix, int PhoneMumber, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserUpdateBlo> Update(int PhoneNumberPrefix, int PhoneMumber, string Password, UserUpdateBlo userUpdateBlo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
