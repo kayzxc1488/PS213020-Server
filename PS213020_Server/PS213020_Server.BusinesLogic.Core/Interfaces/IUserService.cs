8c01f80c7931862111c56e23017381d7d7357e2c
StudentDev <vovaklinov007@gmail.com>
2021-11-27 17:30:42 +0300

    124
    1252346

--- PS213020_Server/PS213020_Server.BusinesLogic.Core/Interfaces/IUserService.cs
+++ PS213020_Server/PS213020_Server.BusinesLogic.Core/Interfaces/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PS213020_Server.BusinesLogic.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace PS213020_Server.BusinesLogic.Core.Interfaces
 {
     public interface IUserService
     {
-        void Auth();
-
+        Task<UserInformationBLo> Auth(int PhoneNumberPrefix, int PhoneMumber, string Password);
+        Task<UserInformationBLo> Regestration(int PhoneNumberPrefix, int PhoneMumber, string Password);
     }
 }
---
+++ PS213020_Server/PS213020_Server.BusinesLogic.Core/Models/UserInformationBLo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS213020_Server.BusinesLogic.Core.Models
+{
+    public class UserInformationBLo
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Introduction { get; set; }
+    }
+}
