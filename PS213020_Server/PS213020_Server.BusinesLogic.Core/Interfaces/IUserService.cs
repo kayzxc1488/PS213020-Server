79278d0da354fc8f7f51f30b9d713b018f5343a3
StudentDev <vovaklinov007@gmail.com>
2021-11-27 15:32:39 +0300

    124235
    262345673457

---
+++ PS213020_Server/PS213020_Server.BusinesLogic.Core/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS213020_Server.BusinesLogic.Core.Interfaces
+{
+    public interface IUserService
+    {
+        void Auth();
+    }
+}
